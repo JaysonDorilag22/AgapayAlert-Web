8f3995588c844f7f39f4d3a4902bf38db3368008
Sadgirlscripter <130391500+sadgirlscripter@users.noreply.github.com>
2025-01-22 17:40:55 +0800

    About UI

--- agapayalert/src/pages/About.jsx
+++ agapayalert/src/pages/About.jsx
@@ -1,16 +1,139 @@
-import React from "react";
-import logo from "../assets/AGAPAYALERT.svg";
+import React, {useState, useEffect} from "react";
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import logoimago from "../assets/AGAPAYALERT - imagotype.svg";
+import makatichief from "../assets/Chiefs/MakatiChief.png";
+import makati from "../assets/pnplogo/MakatiLogo.svg";
+import taguig from "../assets/pnplogo/TaguigLogo.svg";
+import pasay from "../assets/pnplogo/PasayLogo.svg";
+import paranaque from "../assets/pnplogo/ParanaqueLogo.svg";
+import pateros from "../assets/pnplogo/PaterosLogo.svg";
+import tup from "../assets/tup.png";
+import taguigchief from "../assets/Chiefs/taguigchief.png";
+import pasaychief from "../assets/Chiefs/pasaychief.png";
+import paranaquechief from "../assets/Chiefs/paranaquechief.png";
+
+const slides = [
+    {
+      name: "PCOL JEAN I. DELA TORRE",
+      title: "Officer in Charge, Makati CPS",
+      description: "Located in the heart of Makati, one of the Philippines’ busiest business districts, the Makati City Police Station plays a crucial role in maintaining peace and order in a city with a daytime population of over a million people. Under the leadership of PCOL Jean I. Dela Torre, the station ensures the safety of residents, workers, and visitors by addressing various public safety concerns.",
+      image: makatichief
+    },
+    {
+      name: "PCOL JOEY T. GOFORTH",
+      title: "Chief of Police, Taguig CPS",
+      description: "Situated in Taguig, a rapidly growing urban center known for Bonifacio Global City and government institutions, the Taguig City Police Station serves and protects a population of over 1 million residents. Led by PCOL Joey T. Goforth, the station plays a critical role in maintaining safety in one of Metro Manila's most dynamic cities.",
+      image: taguigchief
+    },
+    {
+      name: "PCOL SAMUEL B. PABONITA",
+      title: "Officer in Charge, Pasay CPS",
+      description: "Located in Pasay, a bustling city home to major landmarks such as the Ninoy Aquino International Airport and popular entertainment hubs, the Pasay City Police Station protects a population of over 440,000 residents. Under the leadership of PCOL Samuel B. Pabonita, the station ensures public safety in this vital area, which serves as a gateway to Metro Manila and a connecting point to neighboring cities.",
+      image: pasaychief
+    },
+    {
+      name: "PCOL MELVIN RIATAZA MONTANTE",
+      title: "Chief of Police, Parañaque CPS",
+      description: "Situated in Parañaque, a gateway city near the Ninoy Aquino International Airport, the Parañaque City Police Station is dedicated to safeguarding a population of over 700,000 residents. Led by PCOL Melvin Riataza Montante, the station plays a vital role in ensuring the security of this highly urbanized area, which serves as a key transportation and commercial hub.",
+      image: paranaquechief
+    },
+    {
+      name: "PCOL ANTHONY C. GANTANG",
+      title: "Chief of Police, Pateros MPS",
+      description: "Located in Pateros, the smallest municipality in Metro Manila, the Pateros Municipal Police Station is dedicated to protecting its close-knit population of over 63,000 residents. Under the leadership of PCOL Anthony C. Gantang, the station ensures safety and order in this historic and peaceful town.",
+      image: pateros
+    },
+    {
+      name: "TECHNOLOGICAL UNIVERSITY OF THE PHILIPPINES",
+      title: "TAGUIG CAMPUS",
+      description: "TUP-Taguig is a premier state university located in Taguig City, offering top-notch education in engineering, technology, and applied sciences. Known for producing skilled and innovative professionals, TUP-Taguig plays a vital role in shaping the workforce of Metro Manila and beyond.",
+      image: tup
+    }
+  ];
+
+  const images = [
+    { src: makati, alt: "Makati", index: 0 },
+    { src: taguig, alt: "Taguig", index: 1 },
+    { src: pasay, alt: "Pasay", index: 2 },
+    { src: paranaque, alt: "Paranaque", index: 3 },
+    { src: pateros, alt: "Pateros", index: 4 },
+    { src: tup, alt: "TUP", index: 5 },
+  ];
+
 
 const About = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    useEffect(() => {
+      const timer = setInterval(() => {
+        setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+      }, 8000);
+  
+      return () => clearInterval(timer);
+    }, []);
+  
+    const handlePrevClick = () => {
+      setCurrentSlide((prevSlide) => (prevSlide - 1 + slides.length) % slides.length);
+    };
+  
+    const handleNextClick = () => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+    };
+
     return (
-        <div className="h-screen">
-            <div className="h-full w-full place-items-center">
-                <div className="Container bg-[#123F7B] h-54 w-5/6 place-items-center rounded-3xl overflow-hidden content-center shadow-lg px-44 py-10">
-                    <h1 className="text-white text-4xl pb-6 font-bold">About us</h1>
-                    <p className="text-white text-lg">AgapayAlert is a web application that helps you keep track of your loved ones. It is designed to help you monitor the whereabouts of your family members, especially those who are vulnerable and may need assistance in case of emergencies. With AgapayAlert, you can easily report a missing person and get help from the community to locate them. You can also view reports submitted by other users and help find missing persons in your area.</p>
+    <div className="relative min-h-screen">
+      <div className="h-screen">
+        <div className="h-full w-full place-items-center">
+          <div className="Container bg-[#123F7B] h-[440px] w-full rounded-[75px] shadow-lg px-[24px] overflow-hidden content-center">
+            <div className="grid grid-cols-1 lg:grid-cols-2 justify-between gap-2">
+              <div className="flex flex-col justify-start w-full max-w-xl py-2 pl-4 lg:pl-12 content-center">
+                <p className="text-white text-lg lg:text-2xl font-extrabold">Building Safer Communities Together</p>
+                <p className="text-white text-sm lg:text-base font-base pt-1">Empowering Communities with Real-Time Alerts and Swift Responses.</p>
+                <p className="text-white text-base lg:text-lg font-semibold pt-4">Mission</p>
+                <p className="text-white text-xs lg:text-sm font-light pt-1">To enhance public safety in urban areas by providing a reliable system for rapid incident reporting and response. AgapayReady ensures quick information dissemination, fosters community engagement, and facilitates seamless coordination with law enforcement to protect lives.</p>
+                <p className="text-white text-base lg:text-lg font-semibold pt-4">Vision</p>
+                <p className="text-white text-xs lg:text-sm font-light pt-1">To build safer communities through real-time updates, active public participation, and secure technology, ensuring efficient responses to critical incidents and fostering trust between citizens and law enforcement.</p>
+              </div>
+              <div className="flex flex-col justify-end w-full h-full">
+                <div className="Container bg-white rounded-[75px] place-items-center overflow-hidden">
+                  <img src={logoimago} alt="placeholder" className="object-contain w-[300px] h-[200px] lg:w-[600px] lg:h-[360px]" />
                 </div>
+              </div>
             </div>
+          </div>
+          <div className="Container bg-white h-[600px] w-full mt-4">
+            <div className="relative w-full">
+              <div className="grid grid-cols-1 lg:grid-cols-2 justify-between gap-2">
+                <div className="flex flex-col justify-start w-full pl-4 lg:pl-32 pt-8 lg:pt-16 content-start">
+                  <p className="text-[#123F7B] text-2xl lg:text-4xl font-extrabold">{slides[currentSlide].name}</p>
+                  <p className="text-[#D46A79] text-lg lg:text-2xl font-light pt-1">{slides[currentSlide].title}</p>
+                  <p className="text-[#123F7B] text-sm lg:text-lg font-light pt-4 pr-4 lg:pr-12">{slides[currentSlide].description}</p>
+                </div>
+                <div className="flex flex-col justify-end w-full h-full place-items-center">
+                  <img src={slides[currentSlide].image} alt="placeholder" className="object-contain w-[300px] h-[300px] lg:w-[600px] lg:h-[600px]" />
+                </div>
+              </div>
+              <div className="fixed bottom-0 left-0 w-full px-4 py-2 backdrop-blur-lg bg-[#123F7B] bg-opacity-20 z-50">
+                <div className="flex flex-row h-[100px] lg:h-[150px] w-full justify-between items-center space-x-4">
+                  <FaArrowLeft className="text-white text-xl lg:text-2xl cursor-pointer" onClick={handlePrevClick} />
+                  <div className="flex flex-row justify-center items-center space-x-12 lg:space-x-24">
+                    {images.map((image, index) => (
+                      <img
+                        key={index}
+                        src={image.src}
+                        alt={image.alt}
+                        className={`object-contain ${currentSlide === image.index ? 'w-[75px] h-[75px] lg:w-[125px] lg:h-[125px]' : 'w-[50px] h-[50px] lg:w-[80px] lg:h-[80px]'}`}
+                      />
+                    ))}
+                  </div>
+                  <FaArrowRight className="text-white text-xl lg:text-2xl cursor-pointer" onClick={handleNextClick} />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+    </div>
     );
 };
 
--- agapayalert/src/pages/Home.jsx
+++ agapayalert/src/pages/Home.jsx
@@ -9,89 +9,86 @@ import { FaStar } from 'react-icons/fa';
 const Home = () => {
     return (
         <div className="h-screen">
-            <div className="Container bg-[#123F7B] h-5/6 rounded-[75px]  overflow-hidden content-center justify-between px-44 py-10">
-                <div className="flex flex-row place-items-center justify-between space-x-24">
-                <div className="flex flex-col w-full max-w-lg">
-                    <p className="text-white text-4xl font-extrabold">Hold the Future of Public 
-                    Safety in Your Hands</p>
-                    <p className="text-white text-2xl font-light pt-4">Be a part of the solution—help protect your loved ones and neighbors. Real-time updates, life-saving alerts, and peace of mind in one app.</p>
-                    <div className="py-8">
-                        <button className="bg-white text-[#123F7B] font-normal text-xl px-8 py-4 rounded-full shadow-md hover:bg-white/90 flex items-center space-x-2">
-                            <span>Get The App</span>
-                            <RxArrowRight /> 
-                        </button>
-                    </div>
-                    <div className="flex flex-row space-x-16">
-                        <div className="flex flex-col">
-                            <p className="text-white text-2xl font-extrabold">1,000+</p>
-                            <p className="text-white text-lg font-light">Downloads</p>
-                        </div>
-                        <div className="flex flex-col">
-                            <p className="text-white text-2xl font-extrabold">4.5</p>
-                            <p className="text-white text-lg font-light">Rating</p>
-                        </div>
-                    </div>
+        <div className="Container bg-[#123F7B] h-5/6 rounded-[75px] overflow-hidden content-center justify-between px-4 py-10 lg:px-44 lg:py-10">
+          <div className="flex flex-col lg:flex-row place-items-center justify-between space-y-8 lg:space-y-0 lg:space-x-24">
+            <div className="flex flex-col w-full max-w-lg">
+              <p className="text-white text-3xl lg:text-4xl font-extrabold">Hold the Future of Public Safety in Your Hands</p>
+              <p className="text-white text-xl lg:text-2xl font-light pt-4">Be a part of the solution—help protect your loved ones and neighbors. Real-time updates, life-saving alerts, and peace of mind in one app.</p>
+              <div className="py-8">
+                <button className="bg-white text-[#123F7B] font-normal text-lg lg:text-xl px-6 lg:px-8 py-3 lg:py-4 rounded-full shadow-md hover:bg-white/90 flex items-center space-x-2">
+                  <span>Get The App</span>
+                  <RxArrowRight />
+                </button>
+              </div>
+              <div className="flex flex-row space-x-8 lg:space-x-16">
+                <div className="flex flex-col">
+                  <p className="text-white text-xl lg:text-2xl font-extrabold">1,000+</p>
+                  <p className="text-white text-base lg:text-lg font-light">Downloads</p>
                 </div>
-                <div className="flex flex-col w-full max-w-3xl pl-16">
-                    <img src={mobileMU} alt="placeholder" className="scale-150 translate-y-28 translate-x-28 object-center w-full h-full"/>
+                <div className="flex flex-col">
+                  <p className="text-white text-xl lg:text-2xl font-extrabold">4.5</p>
+                  <p className="text-white text-base lg:text-lg font-light">Rating</p>
                 </div>
-                </div>
+              </div>
             </div>
-            <div className="py-12 px-24">
-                <div className="flex flex-row place-items-center">
-                    <div className="flex flex-col w-full">
-                        <p className="text-3xl font-extrabold">What they say about us</p>
-                        <div className="flex flex-row justify-between w-full space-x-16 pt-2">
-                            <div className="flex flex-col bg-[#123F7B] text-white px-8 py-4 rounded-lg shadow-md w-full max-w-sm">
-                                <div className="flex space-x-1 mt-2 pb-4 place-items-center space-x-2">
-                                    <FaStar className="text-yellow-400" />
-                                    <FaStar className="text-yellow-400" />
-                                    <FaStar className="text-yellow-400" />
-                                    <FaStar className="text-yellow-400" />
-                                    <FaStar className="text-yellow-400" />
-                                    <p className="text-lg font-extrabold">  5.0</p>
-                                </div>
-                                <div className="text-pretty">  
-                                <p className="text-lg font-light pb-4">This app changed my life. It saved so many people and helped those in need.</p>
-                                <p className="text-lg font-bold">John Doe</p>
-                                </div>
-                            </div>
-                            <div className="flex flex-col bg-[#123F7B] text-white px-8 py-4 rounded-lg shadow-md w-full max-w-sm">
-                                <div className="flex space-x-1 mt-2 pb-4 place-items-center space-x-2">
-                                    <FaStar className="text-yellow-400" />
-                                    <FaStar className="text-yellow-400" />
-                                    <FaStar className="text-yellow-400" />
-                                    <FaStar className="text-yellow-400" />
-                                    <FaStar className="text-yellow-400" />
-                                    <p className="text-lg font-extrabold">  5.0</p>
-                                </div>
-                                <div className="text-pretty">  
-                                <p className="text-lg font-light pb-4">This app changed my life. It saved so many people and helped those in need.</p>
-                                <p className="text-lg font-bold">John Doe</p>
-                                </div>
-                            </div>
-                            <div className="flex flex-col bg-[#123F7B] text-white px-8 py-4 rounded-lg shadow-md w-full max-w-sm">
-                                <div className="flex space-x-1 mt-2 pb-4 place-items-center space-x-2">
-                                    <FaStar className="text-yellow-400" />
-                                    <FaStar className="text-yellow-400" />
-                                    <FaStar className="text-yellow-400" />
-                                    <FaStar className="text-yellow-400" />
-                                    <FaStar className="text-yellow-400" />
-                                    <p className="text-lg font-extrabold">  5.0</p>
-                                </div>
-                                <div className="text-pretty">  
-                                <p className="text-lg font-light pb-4">This app changed my life. It saved so many people and helped those in need.</p>
-                                <p className="text-lg font-bold">John Doe</p>
-                                </div>
-                            </div>
-                        </div>
-                            
-                    </div>
-                </div>
-                    
+            <div className="flex flex-col w-full max-w-3xl pl-0 lg:pl-16">
+              <img src={mobileMU} alt="placeholder" className="scale-100 lg:scale-150 lg:translate-y-28 lg:translate-x-28 object-center w-full h-full" />
             </div>
+          </div>
         </div>
+        <div className="py-12 px-4 lg:px-24">
+          <div className="flex flex-col lg:flex-row place-items-center">
+            <div className="flex flex-col w-full">
+              <p className="text-2xl lg:text-3xl font-extrabold">What they say about us</p>
+              <div className="flex flex-col lg:flex-row justify-between w-full space-y-4 lg:space-y-0 lg:space-x-16 pt-2">
+                <div className="flex flex-col bg-[#123F7B] text-white px-8 py-4 rounded-lg shadow-md w-full max-w-sm">
+                  <div className="flex space-x-1 mt-2 pb-4 place-items-center space-x-2">
+                    <FaStar className="text-yellow-400" />
+                    <FaStar className="text-yellow-400" />
+                    <FaStar className="text-yellow-400" />
+                    <FaStar className="text-yellow-400" />
+                    <FaStar className="text-yellow-400" />
+                    <p className="text-lg font-extrabold">  5.0</p>
+                  </div>
+                  <div className="text-pretty">  
+                    <p className="text-lg font-light pb-4">This app changed my life. It saved so many people and helped those in need.</p>
+                    <p className="text-lg font-bold">John Doe</p>
+                  </div>
+                </div>
+                <div className="flex flex-col bg-[#123F7B] text-white px-8 py-4 rounded-lg shadow-md w-full max-w-sm">
+                  <div className="flex space-x-1 mt-2 pb-4 place-items-center space-x-2">
+                    <FaStar className="text-yellow-400" />
+                    <FaStar className="text-yellow-400" />
+                    <FaStar className="text-yellow-400" />
+                    <FaStar className="text-yellow-400" />
+                    <FaStar className="text-yellow-400" />
+                    <p className="text-lg font-extrabold">  5.0</p>
+                  </div>
+                  <div className="text-pretty">  
+                    <p className="text-lg font-light pb-4">This app changed my life. It saved so many people and helped those in need.</p>
+                    <p className="text-lg font-bold">John Doe</p>
+                  </div>
+                </div>
+                <div className="flex flex-col bg-[#123F7B] text-white px-8 py-4 rounded-lg shadow-md w-full max-w-sm">
+                  <div className="flex space-x-1 mt-2 pb-4 place-items-center space-x-2">
+                    <FaStar className="text-yellow-400" />
+                    <FaStar className="text-yellow-400" />
+                    <FaStar className="text-yellow-400" />
+                    <FaStar className="text-yellow-400" />
+                    <FaStar className="text-yellow-400" />
+                    <p className="text-lg font-extrabold">  5.0</p>
+                  </div>
+                  <div className="text-pretty">  
+                    <p className="text-lg font-light pb-4">This app changed my life. It saved so many people and helped those in need.</p>
+                    <p className="text-lg font-bold">John Doe</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
-    };
-
-export default Home;
+  };
+  
+  export default Home;
