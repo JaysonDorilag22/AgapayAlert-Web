a9f828728fb99701895d438d31a63ca0b239927a
Sadgirlscripter <130391500+sadgirlscripter@users.noreply.github.com>
2025-03-14 16:03:52 +0800

    server adjust

--- agapayalert/src/redux/store.js
+++ agapayalert/src/redux/store.js
@@ -27,8 +27,8 @@ const store = configureStore({
 });
 
 //wanel haus
-export const server = "http://localhost:3000/api/v1";
+// export const server = "http://localhost:3000/api/v1";
 
-// export const server = "https://agapayalert-server.onrender.com/api/v1";
+export const server = "https://agapayalert-server.onrender.com/api/v1";
 
 export default store;
